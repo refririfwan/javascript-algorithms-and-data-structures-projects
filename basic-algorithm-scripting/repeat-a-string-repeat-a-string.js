3b3ffdcec624e72cd39e6c31857603964f6d63c5
Refri Rifwan <refririfwan@gmail.com>
2023-08-31 15:58:36 +0700

    repeat-a-string-repeat-a-string

---
+++ basic-algorithm-scripting/repeat-a-string-repeat-a-string.js
@@ -0,0 +1,14 @@
+function repeatStringNumTimes(str, num) {
+  if (num < 0) {
+    return "";
+  }
+
+  let repeatedString = "";
+  for (let i = 0; i < num; i++) {
+    repeatedString += str;
+  }
+
+  return repeatedString;
+}
+
+repeatStringNumTimes("abc", 3);
