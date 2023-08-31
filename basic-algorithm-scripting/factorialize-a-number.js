885ee6cc8e52dcfc6efe1859d266ea2d7dda2ba8
Refri Rifwan <refririfwan@gmail.com>
2023-08-31 11:27:27 +0700

    factorialize-a-number

---
+++ basic-algorithm-scripting/factorialize-a-number.js
@@ -0,0 +1,14 @@
+function factorialize(num) {
+  if (num === 0 || num === 1) {
+    return 1;
+  }
+
+  let result = 1;
+  for (let i = 2; i <= num; i++) {
+    result *= i;
+  }
+
+  return result;
+}
+
+factorialize(5);
